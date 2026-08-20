b23f70fa47c8a5b963370e86c339c3a97cb25903
Bharath Kumar J N <bharathdhanavan2612@gmail.com>
2026-08-20 23:43:45 +0530

    AuthService initiated

--- src/main/java/com/bharath/ecommerceapi/repo/UserRepository.java
+++ src/main/java/com/bharath/ecommerceapi/repo/UserRepository.java
@@ -1,10 +1,16 @@
 package com.bharath.ecommerceapi.repo;
 
 import com.bharath.ecommerceapi.model.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    boolean existsByEmail(String email);
+    Optional<User> findByEmail(@Email(message = "valid email is required!") @NotBlank(message = "mandatory") String email);
 }
--- src/main/java/com/bharath/ecommerceapi/service/impl/AuthServiceImpl.java
+++ src/main/java/com/bharath/ecommerceapi/service/impl/AuthServiceImpl.java
@@ -1,18 +1,67 @@
 package com.bharath.ecommerceapi.service.impl;
 
+import com.bharath.ecommerceapi.exception.BadRequestException;
+import com.bharath.ecommerceapi.model.Cart;
+import com.bharath.ecommerceapi.model.User;
+import com.bharath.ecommerceapi.model.Wishlist;
 import com.bharath.ecommerceapi.model.dto.request.LoginRequest;
 import com.bharath.ecommerceapi.model.dto.request.RegisterRequest;
 import com.bharath.ecommerceapi.model.dto.response.AuthResponse;
+import com.bharath.ecommerceapi.repo.CartRepository;
+import com.bharath.ecommerceapi.repo.UserRepository;
+import com.bharath.ecommerceapi.repo.WishlistRepository;
 import com.bharath.ecommerceapi.service.inf.IAuthService;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class AuthServiceImpl implements IAuthService {
+
+    private final UserRepository userRepository;
+    private final CartRepository cartRepository;
+    private final WishlistRepository wishlistRepository;
+
     @Override
+    @Transactional
     public String register(RegisterRequest request) {
-        return "";
+        if (userRepository.existsByEmail(request.getEmail()))
+            throw new BadRequestException("Email already Exists!");
+        User user = User.builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .email(request.getEmail())
+                .password(request.getPassword())
+                .role(request.getRole())
+                .build();
+
+        User savedUser = userRepository.save(user);
+
+        Cart cart = Cart.builder()
+                .user(savedUser)
+                .build();
+        cartRepository.save(cart);
+
+        Wishlist wishlist = Wishlist.builder()
+                .user(savedUser)
+                .build();
+        wishlistRepository.save(wishlist);
+        return "User Created Successfully!";
     }
 
     @Override
     public AuthResponse login(LoginRequest request) {
-        return null;
+        User user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new BadRequestException("Invalid Email Or Password"));
+
+        if(!user.getPassword().equals(request.getPassword())) {
+            throw new BadRequestException("Invalid Email Or Password!");
+        }
+
+        return AuthResponse.builder()
+                .token("123")
+                .build();
+
     }
 }
